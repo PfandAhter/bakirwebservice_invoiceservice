7b4fe384b340ef63d067eedbce5c61990e453b6b
Ataberk Bakir <bakirr.ataberk@gmail.com>
2026-01-04 23:19:36 +0300

    Add no-args constructor to `UpdateAnalysisInvoiceIdRequest`

--- src/main/java/com/bakirwebservice/invoiceservice/api/request/UpdateAnalysisInvoiceIdRequest.java
+++ src/main/java/com/bakirwebservice/invoiceservice/api/request/UpdateAnalysisInvoiceIdRequest.java
@@ -3,11 +3,13 @@ package com.bakirwebservice.invoiceservice.api.request;
 import com.bakirwebservice.invoiceservice.model.enums.InvoiceStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateAnalysisInvoiceIdRequest {
     private String analysisReportId;
 
